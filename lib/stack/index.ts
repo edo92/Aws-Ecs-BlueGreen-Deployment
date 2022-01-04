386655ce1dff26bf6fc370940272dac83291f9be
Eduard Jacobs <edoter92@gmail.com>
2022-01-04 05:23:48 -0800

    add pipeline to stack

--- lib/stack/index.ts
+++ lib/stack/index.ts
@@ -2,6 +2,7 @@ import * as config from "@config";
 import * as cdk from "@aws-cdk/core";
 import { Resources } from "@pattern/Resources";
 import { EcsService } from "@pattern/EcsService";
+import { Pipeline } from "./pipeline";
 
 export class Infrastructure extends cdk.Stack {
    constructor(scope: cdk.Construct, id: string) {
@@ -17,10 +18,22 @@ export class Infrastructure extends cdk.Stack {
        *
        * Ecs Service
        */
-      new EcsService(this, "Ecs-Service", {
+      const ecsService = new EcsService(this, "Ecs-Service", {
          vpc: resources.vpc,
          elb: resources.loadBalancer,
          names: config.resource_names,
+      });
+
+      /**
+       *
+       * Pipeline
+       */
+      new Pipeline(this, "Service-Pipeline", {
+         region: this.region,
+         account: this.account,
+         service: ecsService,
+         names: config.resource_names,
+         github: config.project_source,
       });
    }
 }
